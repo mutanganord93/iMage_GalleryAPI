8a16859fbe4cca3182a145b84797355a70d5c2e7
Kibill <54875220+mutanganord93@users.noreply.github.com>
2023-03-12 16:49:06 -0700

    nothing

--- fetchData.js
+++ fetchData.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 router.route('/pictures',async(req,res)=>{
     try {
         console.log('on server')
-        const val = req.query.query
+        const val = req.query.query;
         const apiKey = process.env.apiKey;
         const url = `https://api.unsplash.com/search/photos?page=1&per_page=100&query=${val}&client_id=${apiKey}`;
         const response = await axios.get(url);
